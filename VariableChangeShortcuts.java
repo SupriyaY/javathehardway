9cfe8f5dc39f8c517a49976296676e6a0b118511
supriya  yerramilli <supriyayerramilli@supriyas-MacBook-Air.local>
2019-07-03 10:31:10 -0400

    learning java, the hard way, more files

--- VariableChangeShortcuts.java
+++ VariableChangeShortcuts.java
@@ -5,7 +5,24 @@ public class VariableChangeShortcuts {
         i = 5;
         j = 5;
         k = 5;
-        System.out.println( "i: " + i + "\tj: " + )
+        System.out.println( "i: " + i + "\tj: " + j + "\tk: " + k );
+        i = i + 3;
+        j = j - 3;
+        k = k * 3;
+        System.out.println("i: " + i + "\tj: " + j + "\tk: " + k  + "\n");
+        
+        i = 5;
+        j = 5;
+        k = 5;
+        System.out.println("i: " + i + "\tj: " + j + "\tk: " + k );
+        i += 3;
+        j -= 3;
+        k *= 3;
+        System.out.println( "i: " + i + "\tj: " + j + "\tk: " + k + "\n");
+
+        i = j = k = 5;
+        System.out.println()
+
 
     }
 
